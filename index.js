4c3564068eda286f18d9059d830ccedbfdf19131
MominIshtiaq <momin989ishtiaq@gmail.com>
2023-09-22 18:49:08 +0500

    small changes

--- index.js
+++ index.js
@@ -6,20 +6,14 @@ const sendMail = () => {
     To: "momin989ishtiaq@gmail.com",
     From: document.getElementById("email").value,
     Subject: "Testing Contact Us Form",
-    Body: `Name: ${document.getElementById("name").value}
-       <br> Email: ${document.getElementById("email").value}
-       <br> Password: ${document.getElementById("password").value}
-       <br> Message: ${document.getElementById("message").value}`,
+    Body:
+      "Name: " +
+      document.getElementById("name").value +
+      "<br> Email: " +
+      document.getElementById("email").value +
+      "<br> PhoneNo: " +
+      document.getElementById("phone").value +
+      "<br> Message: " +
+      document.getElementById("message").value,
   }).then((message) => alert("Message Sent successfully"));
 };
-// Host: "smtp.gmail.com",
-// Username: "momin989ishtiaq@gmail.com",
-// Password: "0333Momin0333",
-// To: "momin989ishtiaq@gmail.com",
-// From: document.getElementById("email").value,
-// Subject: "Testing Contact Us Form",
-// Body: `Name: ${document.getElementById("name").value}
-//        <br> Email: ${document.getElementById("email").value}
-//        <br> Password: ${document.getElementById("password").value}
-//        <br> Message: ${document.getElementById("message").value}`,
-// }).then((message) => alert("Message Sent Successfully"));
