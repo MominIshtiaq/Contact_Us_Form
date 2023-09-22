e1965110f30745bc91aecde29b0552518973d06b
MominIshtiaq <momin989ishtiaq@gmail.com>
2023-09-22 18:25:42 +0500

    javascirpt bug removal attempt 1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const sendMail = () => {
-  ({
+  Email.send({
     Host: "smtp.gmail.com",
     Username: "momin989ishtiaq@gmail.com",
     Password: "0333Momin0333",
@@ -7,8 +7,19 @@ const sendMail = () => {
     From: document.getElementById("email").value,
     Subject: "Testing Contact Us Form",
     Body: `Name: ${document.getElementById("name").value}
-           <br> Email: ${document.getElementById("email").value}
-           <br> Password: ${document.getElementById("password").value}
-           <br> Message: ${document.getElementById("message").value}`,
-  }).then((message) => alert("Message Sent Successfully"));
+       <br> Email: ${document.getElementById("email").value}
+       <br> Password: ${document.getElementById("password").value}
+       <br> Message: ${document.getElementById("message").value}`,
+  }).then((message) => alert("Message Sent successfully"));
 };
+// Host: "smtp.gmail.com",
+// Username: "momin989ishtiaq@gmail.com",
+// Password: "0333Momin0333",
+// To: "momin989ishtiaq@gmail.com",
+// From: document.getElementById("email").value,
+// Subject: "Testing Contact Us Form",
+// Body: `Name: ${document.getElementById("name").value}
+//        <br> Email: ${document.getElementById("email").value}
+//        <br> Password: ${document.getElementById("password").value}
+//        <br> Message: ${document.getElementById("message").value}`,
+// }).then((message) => alert("Message Sent Successfully"));
