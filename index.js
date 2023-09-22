3c462eecf636c0348cf0bb29cb930e6bb5b21d9c
MominIshtiaq <momin989ishtiaq@gmail.com>
2023-09-22 18:18:09 +0500

    css and javascript file successfully created

---
+++ index.js
@@ -0,0 +1,14 @@
+const sendMail = () => {
+  ({
+    Host: "smtp.gmail.com",
+    Username: "momin989ishtiaq@gmail.com",
+    Password: "0333Momin0333",
+    To: "momin989ishtiaq@gmail.com",
+    From: document.getElementById("email").value,
+    Subject: "Testing Contact Us Form",
+    Body: `Name: ${document.getElementById("name").value}
+           <br> Email: ${document.getElementById("email").value}
+           <br> Password: ${document.getElementById("password").value}
+           <br> Message: ${document.getElementById("message").value}`,
+  }).then((message) => alert("Message Sent Successfully"));
+};
