c8e742a7b71131efd28ece867fdaeb4639caa714
MominIshtiaq <momin989ishtiaq@gmail.com>
2023-09-23 18:14:19 +0500

    debugging attempt no 5

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-function sendMail() {
-  Email.send({
-    Host: "smtp.elasticemail.com",
-    Username: "momin989ishtiaq@gmail.com",
-    Password: "CD2D1D9ACBECD71C2C981CA6A7A8826E29C1",
-    To: "momin989ishtiaq@gmail.com",
-    From: "momin989ishtiaq@gmail.com",
-    Subject: "This is the subject",
-    Body: "And this is the body",
-  }).then((message) => alert(message));
-}
+
